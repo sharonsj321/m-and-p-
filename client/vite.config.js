d52c560b046eae539b192acda028bd71cb8c77e9
sharon <sjsharon1996@gmail.com>
2025-04-15 21:46:35 +0530

    package and vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://m-and-p-backend.vercel.app',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://m-and-p-backend.vercel.app',
+  //       changeOrigin: true,
+  //       rewrite: path => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }
 })
